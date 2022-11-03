8c5360b7f822f899d401421516ba34c1e3278b61
Phillip <phillip@phillipclark.dev>
2022-11-03 18:55:40 -0500

    add margins and padding behavior

--- Drawer.js
+++ Drawer.js
@@ -9,9 +9,11 @@ export default function Drawer({ children, summary, maxWidth = "auto" }) {
 	const styles = {
 		container: {
 			maxWidth: maxWidth,
+			boxShadow: "rgba(149, 157, 165, 0.2) 0px 8px 24px",
 		},
 		summary: {
-			backgroundColor: "beige",
+			backgroundColor: "white",
+			borderRadius: "5px 5px 0px 0px",
 			display: "flex",
 			justifyContent: "space-between",
 			alignItems: "center",
@@ -20,8 +22,14 @@ export default function Drawer({ children, summary, maxWidth = "auto" }) {
 		},
 		content: {
 			maxHeight: "0px",
+			backgroundColor: "white",
+			borderRadius: "0px 0px 5px 5px",
 			overflow: "hidden",
-			transition: "max-height 250ms",
+			transition: "max-height 500ms, margin-bottom 500ms",
+			boxSizing: "border-box",
+		},
+		contentWrapper: {
+			padding: "1rem",
 		},
 		chevron: {
 			height: "1rem",
@@ -35,12 +43,15 @@ export default function Drawer({ children, summary, maxWidth = "auto" }) {
 		const { style: chevronStyle } = chevronBox.current;
 		console.log(contentStyle, chevronStyle.transform);
 		contentStyle.maxHeight === "0px" || contentStyle.maxHeight === ""
-			? (contentStyle.maxHeight = "fit-content")
+			? (contentStyle.maxHeight = "100vh")
 			: (contentStyle.maxHeight = "0px");
 		chevronStyle.transform === "rotate(0deg)" ||
 		chevronStyle.transform === ""
 			? (chevronStyle.transform = "rotate(90deg)")
 			: (chevronStyle.transform = "rotate(0deg)");
+		contentStyle.marginBottom === "0px" || contentStyle.marginBottom === ""
+			? (contentStyle.marginBottom = "1rem")
+			: (contentStyle.marginBottom = "0");
 	};
 	return (
 		<div style={styles.container}>
@@ -53,7 +64,7 @@ export default function Drawer({ children, summary, maxWidth = "auto" }) {
 				></img>
 			</div>
 			<div ref={contentBox} style={styles.content}>
-				{children}
+				<div style={styles.contentWrapper}>{children}</div>
 			</div>
 		</div>
 	);
